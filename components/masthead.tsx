e4c9a35e53f3d47ac75dcb158d5698f830f33477
Htet Oo Naing <htetoonaing.ycc@gmail.com>
2022-07-29 11:17:59 +0630

    add background parallax effect in masthead

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useContext, useRef } from "react";
 import Image from "next/image";
+import { ScrollContext } from "../utils/scroll-observer";
 
 const Masthead: React.FC = () => {
+  const refContainer = useRef<HTMLDivElement>(null);
+  const { scrollY } = useContext(ScrollContext);
+
+  let progress = 0;
+  const { current: elContainer } = refContainer;
+  if (elContainer) {
+    progress = Math.min(1, scrollY / elContainer.clientHeight);
+  }
+
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
+    <div
+      ref={refContainer}
+      className="min-h-screen flex flex-col items-center justify-center sticky top-0 -z-10"
+      style={{
+        transform: `translateY(-${progress * 20}vh)`,
+      }}
+    >
       <video
         autoPlay
         loop
@@ -14,12 +30,7 @@ const Masthead: React.FC = () => {
         <source src="/assets/the-world-needs-heroes.mp4" type="video/mp4" />
       </video>
       <div className="flex-grow-0 pt-10 transition-opacity duration-1000">
-        <Image
-          src="/assets/logo.png"
-          width={60}
-          height={55}
-          alt="logo"
-        />
+        <Image src="/assets/logo.png" width={60} height={55} alt="logo" />
       </div>
       <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex flex-col items-center justify-center">
         <h1 className="mb-6 text-4xl xl:text-5xl">DC</h1>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import AboutUs from "../components/aboutus";
 import Masthead from "../components/masthead";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Software Agency</title>
         <meta name="description" content="Landing Page for Software Agency" />
